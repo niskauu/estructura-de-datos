95ba08d797fbccc051779067a5f5bb31b847a9af
niska <129527484+niskauu@users.noreply.github.com>
2023-05-05 13:05:10 -0400

    ejercicio 7 terminado

--- retos/ej-nro7.dart
+++ retos/ej-nro7.dart
@@ -19,4 +19,8 @@ void main() {
   lista.sort();
   print("La lista ordenada de forma ascendente es: $lista");
   print("La lista ordenada de forma descendente es: ${lista.reversed}");
+  int minimo = lista.reduce(min);
+  int maximo = lista.reduce(max);
+  print("el menor elemento de la lista es: $minimo");
+  print("el mayor elemento de la lista es: $maximo");
 }
